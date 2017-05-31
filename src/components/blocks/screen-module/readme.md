a9dc9b1d24ce6cab8e56c9dc15267eb2ee305ad7
Chris Pearson <c.pearson@vam.ac.uk>
2017-05-31 15:59:42 +0100

    create screen module

---
+++ src/components/blocks/screen-module/readme.md
@@ -0,0 +1,2 @@
+# Screen module 
+This is the screen module used for the whats on screen displays
---
+++ src/components/blocks/screen-module/screen-module.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Screen module',
+  status: 'wip',
+  context: {
+    eventLabel: 'Evening event',
+    eventName: 'Iron and Gold: The intricate ornament of the Zuloagas',
+    eventPrice: '£12.00',
+    eventLocation: 'Room 49',
+    eventTime: '14.00',
+  },
+};
